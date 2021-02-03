8fb2334b78987473f2fab31e410f39f84e0c13f1
gtozeti <78425766+gtozeti@users.noreply.github.com>
2021-02-03 11:26:32 -0300

    Create 1182 - Coluna na Matriz.c

---
+++ Iniciante/C/1182 - Coluna na Matriz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+float M[12][12],soma,media;
+int i,x,j;
+char a;
+scanf("%d %c",&x,&a);
+soma=0;
+    for (i=0;i<12;i++)
+    {
+        for (j=0;j<12;j++)
+        {
+        scanf("%f",&M[i][j]);
+        if (x == j)
+        {
+             soma = soma + M[i][j];
+        }
+        }
+    }
+    if (a == 'S')
+    {
+            printf("%.1f\n",soma);
+
+    }
+    else if (a == 'M')
+    {
+            media = soma/12;
+            printf("%.1f\n",media);
+    }
+
+    else
+    return 0;
+}
