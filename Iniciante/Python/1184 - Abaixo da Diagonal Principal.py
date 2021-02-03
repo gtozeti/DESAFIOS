b122b76b792ff088b0186189979bd410e6731666
gtozeti <78425766+gtozeti@users.noreply.github.com>
2021-02-03 12:29:15 -0300

    Create 1184 - Abaixo da Diagonal Principal.py

---
+++ Iniciante/Python/1184 - Abaixo da Diagonal Principal.py
@@ -0,0 +1,26 @@
+operacao = input()
+matriz = []
+soma = 0
+media = 0
+total = 0
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+if operacao == "S":
+    for x in range(12):
+        for y in range(11):
+           if y < x:
+               soma += float(matriz[x][y])
+    print("%.1f"%soma)
+else:
+    for x in range(12):
+        for y in range(11):
+            if y < x:
+                media += float(matriz[x][y])
+                total += 1
+    media = media/total
+    print("%.1f"%media)
