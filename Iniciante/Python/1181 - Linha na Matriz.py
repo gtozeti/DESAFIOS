5bd12364e6114de2cd1b9f0c9cfc8cbcf100818c
gtozeti <78425766+gtozeti@users.noreply.github.com>
2021-02-03 12:27:12 -0300

    Create 1181 - Linha na Matriz.py

---
+++ Iniciante/Python/1181 - Linha na Matriz.py
@@ -0,0 +1,21 @@
+nlinha = int(input())
+operacao = input()
+matriz = []
+soma = 0
+media = 0
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+if operacao == "S":
+    for x in range(12):
+        soma += float(matriz[nlinha][x])
+    print("%.1f"%soma)
+else:
+    for x in range(12):
+        media += float(matriz[nlinha][x])
+    media = media/12
+    print("%.1f"%media)
