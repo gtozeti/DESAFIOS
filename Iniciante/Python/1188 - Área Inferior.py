b9bfd0a3ff17e5148a04d4ade8ecb5d72510d336
gtozeti <78425766+gtozeti@users.noreply.github.com>
2021-02-03 12:31:36 -0300

    Create 1188 - Área Inferior.py

---
+++ Iniciante/Python/1188 - Área Inferior.py
@@ -0,0 +1,32 @@
+operacao = input()
+matriz = []
+soma = 0
+media = 0
+total = 0
+min = 5
+max = 6
+
+for i in range(12):
+    linha = []
+    for j in range(12):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+if operacao == "S":
+    for x in range(7,12):
+        max += 1
+        min -= 1
+        for y in range(12):
+           if (y > min) and (y < max):
+               soma += float(matriz[x][y])
+    print("%.1f"%soma)
+else:
+    for x in range(7,12):
+        max -= 1
+        min += 1
+        for y in range(12):
+            if (y > min) and (y < max):
+                media += float(matriz[x][y])
+                total += 1
+    media = media/total
+    print("%.1f"%media)
